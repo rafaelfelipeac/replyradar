d093d762ff5a717f4fec45f520929a7655c66200
Rafael Cordeiro <rafaelfelipeac@gmail.com>
2025-04-04 15:09:46 -0300

    Color theme setup.

--- composeApp/src/commonMain/kotlin/com/rafaelfelipeac/replyradar/core/common/ui/theme/ReplyRadarTheme.kt
+++ composeApp/src/commonMain/kotlin/com/rafaelfelipeac/replyradar/core/common/ui/theme/ReplyRadarTheme.kt
@@ -1,28 +1,8 @@
 package com.rafaelfelipeac.replyradar.core.common.ui.theme
 
-import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.graphics.Color
-
-object ReplyRadarTheme {
-    val colors: ReplyRadarColors
-        @Composable
-        get() = LocalReplyRadarColors.current
-}
-
-val ColorScheme.buttonBorderColor: Color
-    @Composable get() = ReplyRadarTheme.colors.buttonBorderColor
-
-val ColorScheme.horizontalDividerColor: Color
-    @Composable get() = ReplyRadarTheme.colors.horizontalDividerColor
-
-val ColorScheme.textFieldPlaceholderColor: Color
-    @Composable get() = ReplyRadarTheme.colors.textFieldPlaceholderColor
-
-val ColorScheme.unselectedTabColor: Color
-    @Composable get() = ReplyRadarTheme.colors.unselectedTabColor
 
 @Composable
 fun ReplyRadarTheme(
