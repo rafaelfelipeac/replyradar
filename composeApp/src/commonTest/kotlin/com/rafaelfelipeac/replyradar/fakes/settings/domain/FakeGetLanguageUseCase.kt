8e98427af05e3e60c1cec988f5d373c95d7b8b2e
Rafael Cordeiro <rafaelfelipeac@gmail.com>
2025-04-07 19:33:15 -0300

    Added unit tests.

---
+++ composeApp/src/commonTest/kotlin/com/rafaelfelipeac/replyradar/fakes/settings/domain/FakeGetLanguageUseCase.kt
@@ -0,0 +1,15 @@
+package com.rafaelfelipeac.replyradar.fakes.settings.domain
+
+import com.rafaelfelipeac.replyradar.core.common.language.AppLanguage
+import com.rafaelfelipeac.replyradar.core.common.language.AppLanguage.SYSTEM
+import com.rafaelfelipeac.replyradar.features.settings.domain.usecase.GetLanguageUseCase
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
+class FakeGetLanguageUseCase : GetLanguageUseCase {
+    var language: AppLanguage = SYSTEM
+
+    override fun getLanguage(): Flow<AppLanguage> {
+        return flowOf(language)
+    }
+}
