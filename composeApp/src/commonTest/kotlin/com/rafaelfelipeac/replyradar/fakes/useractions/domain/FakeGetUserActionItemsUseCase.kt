527463f468ffff4158d29904cce0ef4c1e03f08a
Rafael Cordeiro <rafaelfelipeac@gmail.com>
2025-04-07 19:25:47 -0300

    Added unit tests.

---
+++ composeApp/src/commonTest/kotlin/com/rafaelfelipeac/replyradar/fakes/useractions/domain/FakeGetUserActionItemsUseCase.kt
@@ -0,0 +1,17 @@
+package com.rafaelfelipeac.replyradar.fakes.useractions.domain
+
+import com.rafaelfelipeac.replyradar.features.useractions.domain.model.UserAction
+import com.rafaelfelipeac.replyradar.features.useractions.domain.usecase.GetUserActionItemsUseCase
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class FakeGetUserActionItemsUseCase : GetUserActionItemsUseCase {
+
+    var userActions: List<UserAction> = emptyList()
+    var shouldThrow: Boolean = false
+
+    override suspend fun getUserActions(): Flow<List<UserAction>> = flow {
+        if (shouldThrow) throw RuntimeException("Fake error.")
+        emit(userActions)
+    }
+}
