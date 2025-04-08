d3bad785f02ad99de709a1919c72752ccf225f27
Rafael Cordeiro <rafaelfelipeac@gmail.com>
2025-04-07 21:08:36 -0300

    PR fixes.

--- composeApp/src/commonMain/kotlin/com/rafaelfelipeac/replyradar/core/common/ui/components/ReplyRadarError.kt
+++ composeApp/src/commonMain/kotlin/com/rafaelfelipeac/replyradar/core/common/ui/components/ReplyRadarError.kt
@@ -4,14 +4,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.style.TextAlign
+import com.rafaelfelipeac.replyradar.core.common.strings.LocalReplyRadarStrings
 
 @Composable
 fun ReplyRadarError(errorMessage: String?) {
     Text(
-        text = errorMessage ?: GENERIC_ERROR_MESSAGE,
+        text = errorMessage ?: LocalReplyRadarStrings.current.genericErrorMessage,
         textAlign = TextAlign.Center,
         style = MaterialTheme.typography.headlineSmall
     )
 }
-
-const val GENERIC_ERROR_MESSAGE = ""
